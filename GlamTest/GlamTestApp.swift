d33b1a04f08bfaded175762beddfb50ec17b5afe
Yaroslav <yaroslav.yashin@me.com>
2023-03-28 22:33:27 +0200

    Initial Commit

---
+++ GlamTest/GlamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlamTestApp.swift
+//  GlamTest
+//
+//  Created by Yaroslav Yashin on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct GlamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
